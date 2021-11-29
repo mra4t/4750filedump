223a7534b52e4cbb005726abea974a15e06d9266
clinkous18 <34517748+clinkous18@users.noreply.github.com>
2021-11-29 17:26:31 -0500

    added css to addfriend & changed filters on explore

--- explore.php
+++ explore.php
@@ -1,12 +1,12 @@
 <html>
 <head>
 <link rel="stylesheet" type="text/css" href="style.css">
-    <div class="navbar">
-        <a href="http://localhost/FINALProject/home.html">Home</a>
-        <a href="http://localhost/FINALProject/explore.php">Explore</a>
-        <a href="http://localhost/FINALProject/addfriend.html">Add Friend</a>
-        <a href="http://localhost/FINALProject/profile.php" class="right">Profile</a>
-    </div>
+   <div class="navbar">
+      <li><a href="http://localhost/FINALProject/home.html">Home</a></li>
+      <li><a href="http://localhost/FINALProject/explore.php">Explore</a></li>
+      <li><a href="http://localhost/FINALProject/addfriend.html">Add Friend</a></li>
+      <li><a href="http://localhost/FINALProject/profile.php">Back</a></li>
+     </div>
     <title>Friend Finder</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -25,18 +25,34 @@
     <p>Find friends in your classes!</p>
     <?php 
         session_start();
-        $db = mysqli_connect('localhost', 'root', '', '4750_project');
+        $db = mysqli_connect('localhost', 'root', '', '4750 Project');
         $sql = "SELECT student.name, student.year, course_id, section FROM Takes NATURAL JOIN student";
         if (isset($_POST['original_courses'])){
             $sql = "SELECT student.name, student.year, course_id, section FROM Takes NATURAL JOIN student";
+            $res = $db->query($sql);
         }
         if (isset($_POST['submitYear'])){
-            $sql = "SELECT student.name, student.year, course_id, section FROM Takes NATURAL JOIN student WHERE student.year = '$_POST[yearChosen]'";
+            $sql1 = "UPDATE student SET show_row='yes' WHERE year='$_POST[yearChosen]'";
+            $sql2 = "UPDATE student SET show_row='no' WHERE year!='$_POST[yearChosen]'";
+            $sql = "SELECT student.name, student.year, course_id, section FROM Takes NATURAL JOIN student WHERE student.show_row = 'yes'";
+            $db->query($sql1);
+            $db->query($sql2);
+            $res = $db->query($sql);
         }
         if (isset($_POST['submitCourse'])){
             $sql = "SELECT student.name, student.year, course_id, section FROM Takes NATURAL JOIN student WHERE course_id = '$_POST[classChosen]'";
+            $sql = "EXEC SelectSpecificCourses @coursePrefix = '$_POST[classChosen]'";
+            $res = $db->query($sql);
         }
-        $res = $db->query($sql);
+        if (isset($_POST['submitGrounds'])){
+            if ($_POST['is_on_grounds'] == "on"){
+                $sql = "SELECT student.name, student.year, course_id, section FROM takes NATURAL JOIN student NATURAL JOIN livesin NATURAL JOIN address WHERE is_on_grounds = 1";
+            } else {
+                $sql = "SELECT student.name, student.year, course_id, section FROM takes NATURAL JOIN student NATURAL JOIN livesin NATURAL JOIN address WHERE is_on_grounds = 0";
+            }
+            $res = $db->query($sql);
+        }
+        
         if($res->num_rows > 0){
             echo"<table><tr><th>Name</th><th>Year</th><th>Course ID</th><th>Section</th></tr>";
             while($row = $res->fetch_assoc()){
@@ -76,8 +92,19 @@
           <input type="number" name="yearChosen">
         </div>
         <div class="input-group">
-          <label>Submit Major</label>
+          <label>Submit Year</label>
           <input type="submit" class="btn" name="submitYear">
+        </div>
+        <div class="input-group">
+            <label>On or off grounds?</label>
+            <select name="is_on_grounds">
+                <option value="off">Off Grounds</option>
+                <option value="on">On Grounds</option>
+            </select>
+        </div>
+        <div class="input-group">
+          <label>Submit On or Off Grounds</label>
+          <input type="submit" class="btn" name="submitGrounds">
         </div>
         <div class="input-group">
           <label>Course Name</label>
